9cb91120034fab042217f85a49f4b10dd8ac9b13
Per Ploug <per.ploug@zalando.de>
2019-01-22 23:43:52 +0100

    Support license lookup in pip
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- src/managers/pip.ts
+++ src/managers/pip.ts
@@ -1,23 +1,54 @@
 import { IDependencyManager } from "../interfaces/IDependencyManager";
 import { IDependency } from "../interfaces/IDependency";
+import { IDependencyLookUp } from "../interfaces/IDependencyLooKUp";
+import fetch from "node-fetch";
 
 export class Pip implements IDependencyManager{
   name = "Pip";
   globs = ["requirements*.txt"]
 
-  async lookup(dependencies : Array<IDependency>){
+  async lookup(dependencies : Array<IDependencyLookUp>){
 
-    return dependencies;
+    var baseUrl = "https://pypi.org/pypi/"
+    var tasks = dependencies.map( async dep => {
+      var lookUp : IDependencyLookUp = {...dep};
+      
+      try{
+        var pipInfo = await fetch(baseUrl + lookUp.name + "/json");
+        
+        if(pipInfo.status === 200){
+          lookUp.found = true;
+          var pipInfoJson = await pipInfo.json();
+
+          lookUp.license = pipInfoJson.info.license;
+          lookUp.latestVersion = pipInfoJson.info.version;
+
+        }else{
+          lookUp.found = false;
+        }  
+      }catch(ex){
+
+      }
+
+      return lookUp;
+    })
+
+    var lookUps = await Promise.all(tasks);
+    return lookUps;
   }
 
   async detect(manifest : string){
-    var foundDependencies = new Array<IDependency>();
+    var deps = manifest
+                .split("\n")
+                .map(x => x.split("=="))
+                .map(x => {
+                  var idep : IDependency = {name: x[0]};
+                  if(x.length == 2){
+                    idep.version = x[1];
+                  }
+                  return idep;
+                });
 
-  
-    //load manifest string and seperate lines
-    //load dependencies and devdependencies as objects
-    //lookup with npm api
-
-    return foundDependencies;
+    return deps;
   }
 }
