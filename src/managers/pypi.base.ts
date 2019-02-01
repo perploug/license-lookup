57370be6e928e189030f5d662459343b846c9d37
Per Ploug <per.ploug@zalando.de>
2019-02-01 10:49:23 +0100

    Rename Base Managers to make it more clear
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

---
+++ src/managers/pypi.base.ts
@@ -0,0 +1,37 @@
+import { IDependencyLookUp } from "../interfaces/IDependencyLooKUp";
+const correct = require('spdx-correct')
+import fetch from "node-fetch";
+
+export class PypiBase {
+  
+  async lookup(dependencies : Array<IDependencyLookUp>){
+
+    var baseUrl = "https://pypi.org/pypi/"
+    var tasks = dependencies.map( async dep => {
+      var lookUp : IDependencyLookUp = {...dep};
+      
+      try{
+        var pipInfo = await fetch(baseUrl + lookUp.name + "/json");
+        
+        if(pipInfo.status === 200){
+          lookUp.found = true;
+          lookUp.url = "https://pypi.org/project/" + lookUp.name;
+
+          var pipInfoJson = await pipInfo.json();
+
+          lookUp.license = correct(pipInfoJson.info.license, { upgrade: false });
+          lookUp.latestVersion = pipInfoJson.info.version;
+
+        }else{
+          lookUp.found = false;
+        }  
+      }catch(ex){
+
+      }
+
+      return lookUp;
+    })
+
+    var lookUps = await Promise.all(tasks);
+    return lookUps;
+  }
