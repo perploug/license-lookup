12dfb89419f15f634b418539e66b81000d43dd5d
Per Ploug <per.ploug@zalando.de>
2019-03-19 09:32:27 +0100

    Fix pom manager undefined dependency exception
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- src/managers/pom.ts
+++ src/managers/pom.ts
@@ -6,32 +6,37 @@ const pomparser = require("pom-parser");
 
 export class Pom extends MavenBase implements IDependencyManager {
   name = "Pom";
-  globs = ["pom.xml"]
+  globs = ["pom.xml"];
 
-  async detect(manifest : string){
-
+  async detect(manifest: string) {
     return new Promise<Array<IDependency>>(function(resolve, reject) {
-      var opts = {xmlContent: manifest};
-      pomparser.parse(opts, function(err : string, pom : any){
-        var model = pom.pomObject.project.dependencies.dependency;
-        
-        var detected : IDependency[] = [];
-        for(const dep of model){
-         
+      var opts = { xmlContent: manifest };
+      pomparser.parse(opts, function(err: string, pom: any) {
+        var detected: IDependency[] = [];
+
+        if (
+          pom &&
+          pom.pomObject &&
+          pom.pomObject.project &&
+          pom.pomObject.project.dependencies &&
+          pom.pomObject.project.dependencies.dependency
+        ) {
+          var model = pom.pomObject.project.dependencies.dependency;
+          for (const dep of model) {
             // extract name and clean it up
-            var name : string = dep.artifactid.replace(/[^0-9a-z.:\-\_]/gi, '');
-          
+            var name: string = dep.artifactid.replace(/[^0-9a-z.:\-\_]/gi, "");
+
             // if the group has a group id, include this in the fully qualified name
-            if(dep.groupid && dep.groupid !== '' && name.indexOf(":") < 0){
+            if (dep.groupid && dep.groupid !== "" && name.indexOf(":") < 0) {
               name = dep.groupid + ":" + name;
             }
-            
-            detected.push({name: name, version: dep.version});
+
+            detected.push({ name: name, version: dep.version });
+          }
         }
 
         resolve(detected);
       });
-      
     });
   }
 }
